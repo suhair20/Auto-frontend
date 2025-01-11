4753d7af2d58ce7a268f80c863ffa5367a157901
suhair20 <moidheensuhair@gmail.com>
2025-01-11 13:22:16 +0530

    css

--- src/slices/userSlice.js
+++ src/slices/userSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 
 
-const baseQuery=fetchBaseQuery({baseUrl:"http://localhost:5000"})
+const baseQuery=fetchBaseQuery({baseUrl:"https://auto-backend-jq5w.onrender.com"})
 const Users_URL='/user'
 
 export const userSlice=createApi({
