64fcb4f1d787bd4bf1f8b73e3b17c67925ba5606
suhair20 <moidheensuhair@gmail.com>
2025-01-14 20:44:47 +0530

    movement

--- src/Components/User/Loading.jsx
+++ src/Components/User/Loading.jsx
@@ -6,7 +6,7 @@ const Loading = () => (
   <div className="loading-overlay bg-navbar-color">
     <div className="auto-container">
       <img src="./banner.png" alt="Auto Rickshaw" className="auto-rickshaw" />
-      
+      <p className='pp  text-white ' > heloo</p>
     </div>
    
   </div>
