a39c4d155ffefda9bbdb64986ac7d713e76bd209
suhair20 <moidheensuhair@gmail.com>
2025-03-14 09:07:45 +0530

    small-changes

--- src/slices/driverSlice.js
+++ src/slices/driverSlice.js
@@ -1,6 +1,9 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const baseQuery=fetchBaseQuery({baseUrl:"http://localhost:5000",
+// const baseQuery=fetchBaseQuery({baseUrl:"http://localhost:5000",
+//     credentials:"include"
+//   })
+  const baseQuery=fetchBaseQuery({baseUrl:"https://auto-backend-jq5w.onrender.com",
     credentials:"include"
   })
 
